e2f6ad94d19772144408795f597390e7aaac4e05
Owapromise <156118281+Owapromise@users.noreply.github.com>
2024-06-20 16:25:06 -0400

    Update one.py

--- one.py
+++ one.py
@@ -20,4 +20,27 @@ while True:
     else:
         print('invalid,try again')
 
-print(players)
+max_score = 50
+player_score = [0 for _ in range(players)]
+
+while max(player_score) < max_score:
+
+    for player_idx in range(players):
+        print('\nPlayer number', player_idx + 1, 'turn has just started! \n')
+        current_score = 0
+
+        while True:
+            should_roll = input('would you like to roll (y)?: ')
+            if should_roll.lower() != 'y':
+                break
+            value = roll()
+            if value == 1:
+                print('you rolled a 1! done')
+                break
+            else:
+                current_score += value
+                print(f'you rolled a:{value}')
+            print('your score is: {current_score}')
+
+        player_score[player_idx] += current_score
+        print('your total score is : ', player_score[player_idx])
