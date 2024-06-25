6018ea1ac056618c6046e4bec02f2d4abd88ebbc
Owapromise <156118281+Owapromise@users.noreply.github.com>
2024-06-25 16:40:10 -0400

    Update three.py

--- three.py
+++ three.py
@@ -6,6 +6,7 @@ import random
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
 
 def generate_problem():
     left = random.randint(MIN_OPERAND,MAX_OPERAND)
@@ -13,7 +14,6 @@ def generate_problem():
     operator = random.choice(OPERATORS)
 
     expr = str(left) + " " + operator + " " + str(right)
-    print(expr)
-    return expr
+    answer = eval(expr)
+    return expr, answer
 
-generate_problem()
