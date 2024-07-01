abd2df2bfc706513d478155aacfe38dd6d10ebdb
Owapromise <156118281+Owapromise@users.noreply.github.com>
2024-07-01 11:26:25 -0400

    Update hanger.py

--- hanger.py
+++ hanger.py
@@ -5,14 +5,31 @@ the player guess the word
 """
 
 import random
+def guess_game():
+    secret_words=['color', 'book', 'ramp', 'group']
+    secret_word = random.choice(secret_words)
+    print(secret_word[0],"___", "___", secret_word[-1])
 
-secret_words=['color', 'book', 'ramp', 'group']
+    score =0
+    user_guess = input("guess the secret word: ")
 
-secret_word = random.choice(secret_words)
-print(secret_word[0],"___", "___", secret_word[-1])
-user_guess = input("guess the secret word: ")
+    while True:
+        if user_guess == secret_word:
+            print("you win. word is ", secret_word)
+            score +=1        
+        else:
+            print("try again")
+        
+        play_again =input("play again?(y/n) ")
+        if play_again == "y":
+            secret_words=['color', 'book', 'ramp', 'group']
+            secret_word = random.choice(secret_words)
+            print(secret_word[0],"___", "___", secret_word[-1])
+            user_guess = input("guess the secret word: ")
+        else:
+            print("thanks for trying")
+            break
+    print("your score is: ", score)
 
-if user_guess == secret_word:
-    print("you win. word is ", secret_word)
-else:
-    print("try again")
+guess_game()
+
